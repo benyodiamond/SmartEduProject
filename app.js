13621ee3092d1d37035443eb40dd0e3d9c11ba15
Bünyamin Elmas <belmas.ben@outlook.com>
2022-08-09 11:01:04 +0300

    Lesson24

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
 
 const app = express();
 
+//CONNECT DB
+
+mongoose
+  .connect('mongodb://127.0.0.1/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB CONNECTED Successfuly');
+  });
 
 // TEMPLE ENGINE
-app.set('view engine', "ejs");
-
+app.set('view engine', 'ejs');
 
 // MIDDLEWARES
-app.use(express.static("public"));
-
+app.use(express.static('public'));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //ROUTE
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: "index"
-  });
-});
-app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-        page_name: "about"
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/category', categoryRoute);
 
-    });
-  });
-  
 const port = 3000;
 app.listen(port, () => {
   console.log(`App stated on port ${port}`);
